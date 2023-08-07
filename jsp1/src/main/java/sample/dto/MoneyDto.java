e8b6e96dbd05210c5fcbc88646665baf6b55cc67
kimsohee-around <koreait.sec2020@gmail.com>
2023-08-07 10:43:06 +0900

    jsp_day1 시작

---
+++ jsp1/src/main/java/sample/dto/MoneyDto.java
@@ -0,0 +1,19 @@
+package sample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+public class MoneyDto {
+   
+   int custno;
+   String custname;
+   String grade;
+   int total;
+   
+}
